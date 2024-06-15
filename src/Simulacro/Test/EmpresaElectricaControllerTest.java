6c5b49c657c1738ba0e08b932354a41a6d21a739
Fedegewis <Fedegewis@gmail.com>
2024-06-15 11:51:20 -0300

    test Simulacro

--- src/Simulacro/Test/EmpresaElectricaControllerTest.java
+++ src/Simulacro/Test/EmpresaElectricaControllerTest.java
@@ -1,6 +1,7 @@
 package Simulacro.Test;
 
 import Simulacro.Controller.EmpresaElectricaController;
+import Simulacro.DTO.UsuarioDTO;
 import Simulacro.DTO.UsuarioIndustrialDTO;
 import Simulacro.DTO.UsuarioResidencialDTO;
 import org.junit.jupiter.api.Test;
@@ -38,38 +39,60 @@ class EmpresaElectricaControllerTest {
     @Test
     void existeUsuarioIndustrial() {
         empresaElectricaController=EmpresaElectricaController.getInstance();
-        UsuarioIndustrialDTO dto=new UsuarioIndustrialDTO("Cabildo","44","4","20","1459","Caba","Buenos Aires","114","489","45","A");
+        UsuarioIndustrialDTO dto=new UsuarioIndustrialDTO("Cabildo","44","4","20","1459","Caba","Buenos Aires","114","4899","45","A");
         UsuarioIndustrialDTO dto2=new UsuarioIndustrialDTO("Cabildo","44","4","20","1459","Caba","Buenos Aires","1144","4895","45","A");
         empresaElectricaController.crearUsuarioInduntrial(dto);
         empresaElectricaController.crearUsuarioInduntrial(dto2);
         boolean res1=empresaElectricaController.existeUsuarioIndustrial(dto.getCuit());
         boolean res2= empresaElectricaController.existeUsuarioIndustrial(dto2.getCuit());
         boolean res3=empresaElectricaController.existeUsuarioIndustrial("112");
-        assertEquals(true,res1);
-        assertEquals(true,res2);
-        assertEquals(false,res3);
+        assertTrue(res1);
+        assertTrue(res2);
+        assertFalse(res3);
     }
 
     @Test
     void existeUSuarioResidencia() {
         empresaElectricaController= EmpresaElectricaController.getInstance();
-        UsuarioResidencialDTO dto = new UsuarioResidencialDTO("Cabildo", "1120", "2", "2", "1426", "CABA", "Bs As", "Federico", "444");
+        UsuarioResidencialDTO dto = new UsuarioResidencialDTO("Cabildo", "1120", "2", "2", "1426", "CABA", "Bs As", "Federico", "4448");
         UsuarioResidencialDTO dto2 = new UsuarioResidencialDTO("Cabildo", "1120", "2", "2", "1426", "CABA", "Bs As", "Federico", "4444");
         empresaElectricaController.crearUsuarioResidencial(dto);
         empresaElectricaController.crearUsuarioResidencial(dto2);
         boolean res1=empresaElectricaController.existeUSuarioResidencia(Integer.parseInt(dto.getDni()));
         boolean res2= empresaElectricaController.existeUSuarioResidencia(Integer.parseInt(dto2.getDni()));
         boolean res3= empresaElectricaController.existeUSuarioResidencia(1245);
-        assertEquals(true,res1);
-        assertEquals(true,res2);
-        assertEquals(false,res3);
+        assertTrue(res1);
+        assertTrue(res2);
+        assertFalse(res3);
     }
 
     @Test
     void buscarUsuarioPorDni() {
+        empresaElectricaController= EmpresaElectricaController.getInstance();
+        UsuarioResidencialDTO dto = new UsuarioResidencialDTO("Cabildo", "1120", "2", "2", "1426", "CABA", "Bs As", "Federico", "4447");
+        UsuarioResidencialDTO dto2 = new UsuarioResidencialDTO("Cabildo", "1120", "2", "2", "1426", "CABA", "Bs As", "Federico", "44441");
+        empresaElectricaController.crearUsuarioResidencial(dto);
+        empresaElectricaController.crearUsuarioResidencial(dto2);
+        UsuarioDTO dtobuscar1=empresaElectricaController.buscarUsuarioPorDni(Integer.parseInt(dto.getDni()));
+        UsuarioDTO dtobuscar2 =empresaElectricaController.buscarUsuarioPorDni(Integer.parseInt(dto2.getDni()));
+        UsuarioDTO dtobuscar3 =empresaElectricaController.buscarUsuarioPorDni(112);
+        assertNotEquals(null,dtobuscar1);
+        assertNotEquals(null,dtobuscar2);
+        assertNull(dtobuscar3);
     }
 
     @Test
     void buscarUsuarioPorCuit() {
+        empresaElectricaController=EmpresaElectricaController.getInstance();
+        UsuarioIndustrialDTO dto=new UsuarioIndustrialDTO("Cabildo","44","4","20","1459","Caba","Buenos Aires","114","4892","45","A");
+        UsuarioIndustrialDTO dto2=new UsuarioIndustrialDTO("Cabildo","44","4","20","1459","Caba","Buenos Aires","1144","48951","45","A");
+        empresaElectricaController.crearUsuarioInduntrial(dto);
+        empresaElectricaController.crearUsuarioInduntrial(dto2);
+        UsuarioDTO dtobuscar1=empresaElectricaController.buscarUsuarioPorCuit(dto.getCuit());
+        UsuarioDTO dtoBuscar2=empresaElectricaController.buscarUsuarioPorCuit(dto2.getCuit());
+        UsuarioDTO dtoBuscar3=empresaElectricaController.buscarUsuarioPorCuit("125");
+        assertNotEquals(null,dtobuscar1);
+        assertNotEquals(null,dtoBuscar2);
+        assertNull(dtoBuscar3);
     }
 }
